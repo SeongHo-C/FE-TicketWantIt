f086c266bb374d713605c4333fef3ec3676fb37c
seongho <leeseong7130@gmail.com>
2023-04-21 01:18:29 +0900

    Feat: 주문 추가(진행) 기능 추가

--- views/order/order.js
+++ views/order/order.js
@@ -1,3 +1,32 @@
+function onLoad() {
+  const tickets_info = JSON.parse(localStorage.getItem('ticket_order'));
+
+  for (let ticket_info of tickets_info) {
+    const ticket = createTicket(ticket_info);
+    orderList.innerHTML += ticket;
+  }
+}
+
+function createTicket(ticket) {
+  const { imageUrl, productName, place, price, quantity = 1 } = ticket;
+
+  return `<li class="ticket">
+  <img
+    class="ticket_img"
+    src=${imageUrl}
+    alt="샘플 이미지"
+  />
+  <div class="ticket_info">
+    <a href="/">${productName}</a>
+    <div class="ticket_sub_info">
+      <p>장소: ${place}</p>
+      <p>수량: ${quantity}개</p>
+    </div>
+    <p class="ticket_money">${(price * quantity).toLocaleString()}원</p>
+  </div>
+</li>`;
+}
+
 function execDaumPostcode() {
   new daum.Postcode({
     oncomplete: function (data) {
@@ -6,7 +35,12 @@ function execDaumPostcode() {
   }).open();
 }
 
+const orderList = document.querySelector('.order_list');
 const addressSearchBtn = document.querySelector('#addressSearchBtn');
+
+window.addEventListener('load', () => {
+  onLoad();
+});
 
 addressSearchBtn.addEventListener('click', () => {
   execDaumPostcode();
