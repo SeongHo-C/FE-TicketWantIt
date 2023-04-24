9a1176f53c4201463f789358b35d7547fe385ba0
seongho <leeseong7130@gmail.com>
2023-04-25 04:35:15 +0900

    Feat: axios interceptor 추가

---
+++ modules/axios_interceptor.js
@@ -0,0 +1,29 @@
+const instance = axios.create({
+  baseURL: 'http://34.64.112.166',
+});
+
+instance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+      config.headers['Content-Type'] = 'application/json; charset=utf-8';
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+instance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default instance;
