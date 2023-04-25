a8fc3ba1b701f5ccdd41bfc66cd9308b25df557e
younu-Yun <87592697+younu-Yun@users.noreply.github.com>
2023-04-25 12:45:47 +0900

    Feat: 관리자 상품목록 수정기능

--- views/admin/admin_edit.js
+++ views/admin/admin_edit.js
@@ -1,10 +1,9 @@
 "use strict";
 
+const imageUrl = document.querySelector("#goodsImage");
+const formUrlInput = document.querySelector(".goods_image");
 
-const imageUrl = document.querySelector('#goodsImage');
-const formUrlInput = document.querySelector('.goods_image');
-
-imageUrl.addEventListener('change', function () {
+imageUrl.addEventListener("change", function () {
     const imageValue = imageUrl.value;
     formUrlInput.value = imageValue;
 });
@@ -24,97 +23,100 @@ imageUrl.addEventListener('change', function () {
 2. edit.html에 비어있는 값에 해당 상품상세정보 GET으로 가져와서 전체 삽입
 */
 
+const form = document.querySelector(".goods_form form");
 const updateBtn = document.querySelector('.goods_form input[type="submit"]');
 
+const productName = document.querySelector("#goodsName");
+const category = document.querySelector("#goodsCate");
+const price = document.querySelector("#goodsPrice");
+const place = document.querySelector("#goodsPlace");
+const speciesAge = document.querySelector("#goodsAge");
+const description = document.querySelector("#goodsDesc");
+const startDate = document.querySelector("#goodsDateStart");
+const endDate = document.querySelector("#goodsDateEnd");
 
+const url = new URL(window.location.href);
+const urlParams = url.searchParams;
+const urlProductId = urlParams.get("productId");
+console.log(urlProductId);
 
-const productName = document.querySelector('#goodsName');
-const category = document.querySelector('#goodsCate')
-const price = document.querySelector('#goodsPrice');
-const place = document.querySelector('#goodsPlace');
-const speciesAge = document.querySelector('#goodsAge');
-const description = document.querySelector('#goodsDesc');
-const startDate = document.querySelector('#goodsDateStart');
-const endDate = document.querySelector('#goodsDateEnd');
+const product = JSON.parse(localStorage.getItem("product"));
+
+if (product !== null) {
+    category.value = product.category;
+    productName.value = product.name;
+    price.value = product.price;
+    description.value = product.description;
+    startDate.value = product.startDate;
+    endDate.value = product.endDate;
+    speciesAge.value = product.speciesAge;
+    place.value = product.place;
+
+    formUrlInput.value = product.image;
+}
+
+localStorage.removeItem("product");
 
 const goodsUpdateApi = async (e) => {
     e.preventDefault();
 
+    const formData = new FormData();
 
-    const url = new URL(window.location.href);
-    const urlParams = url.searchParams;
-    const urlProductId = urlParams.get('productId');
-    console.log(urlProductId)
+    formData.append("category", category.value);
+    formData.append("productName", productName.value);
+    formData.append("imageUrl", imageUrl.files[0]);
+    formData.append("price", price.value);
+    formData.append("place", place.value);
+    formData.append("speciesAge", speciesAge.value);
+    formData.append("description", description.value);
+    formData.append("startDate", startDate.value);
+    formData.append("endDate", endDate.value);
 
     if (urlProductId !== null) {
-        console.log('상품수정')
+        console.log("상품수정");
 
         try {
+            const response = await axios.put(
+                `http://34.64.112.166/api/admin_product/edit?productId=${urlProductId}`,
+                formData
+            );
+            console.log("상품이 수정되었습니다:", response.data);
 
-            /*
-                1. 이 전페이지에서 localStorage로 데이터값 다 저장
-                2. 여기서 input값으로 뿌리기
-                3. PUT으로 수정된 값 보내기
-            */
-
-            /* 
-                put data에 들어가야 할 것들: 
-                상품명, 이미지, 가격, 장소, 연령, 설명, 시작일, 종료일
-            */
-            const modifyApi = {
-                category: String(category.value),
-                productName: String(productName.value),
-                imageUrl: String(imageUrl.value),
-                price: Number(price.value),
-                place: String(place.value),
-                speciesAge: String(speciesAge.value),
-                description: String(description.value),
-                startDate: String(startDate.value),
-                endDate: String(endDate.value),
-            }
-
-            const response = await axios.PUT(`http://10.10.6.158:5000/api/admin_product/edit?productId=${urlProductId}`, modifyApi);
-            console.log('상품이 수정되었습니다:', response.data);
-
-            window.location.href = './goods.html';
-
+            window.location.href = "./goods.html";
         } catch (error) {
-            console.error('상품 수정 중 오류가 발생했습니다:', error);
+            console.error("상품 수정 중 오류가 발생했습니다:", error);
         }
-
     } else {
-        console.log('상품추가')
+        console.log("상품추가");
         /* 
             post data에 들어가야 할 것들: 
-            상품명, 이미지, 가격, 장소, 연령, 설명, 시작일, 종료일
+            카테고리, 상품명, 이미지, 가격, 장소, 연령, 설명, 시작일, 종료일
         */
-        const updateApi = {
-            category: String(category.value),
-            productName: String(productName.value),
-            imageUrl: String(imageUrl.value),
-            price: Number(price.value),
-            place: String(place.value),
-            speciesAge: String(speciesAge.value),
-            description: String(description.value),
-            startDate: String(startDate.value),
-            endDate: String(endDate.value),
-        }
+
+        // const updateApi = {
+        //     category: String(category.value),
+        //     productName: String(productName.value),
+        //     imageUrl: String(imageUrl.value),
+        //     price: Number(price.value),
+        //     place: String(place.value),
+        //     speciesAge: String(speciesAge.value),
+        //     description: String(description.value),
+        //     startDate: String(startDate.value),
+        //     endDate: String(endDate.value),
+        // };
 
         try {
-            const response = await axios.post('http://10.10.6.158:5000/api/admin_product/add', updateApi);
-            console.log('새 상품이 추가되었습니다:', response.data);
+            const response = await axios.post(
+                "http://34.64.112.166/api/admin_product/add",
+                formData
+            );
+            console.log("새 상품이 추가되었습니다:", response.data);
 
-            window.location.href = './goods.html';
-
+            window.location.href = "./goods.html";
         } catch (error) {
-            console.error('상품 추가 중 오류가 발생했습니다:', error);
+            console.error("상품 추가 중 오류가 발생했습니다:", error);
         }
     }
-
 };
 
-updateBtn.addEventListener('click', goodsUpdateApi);
-
-
-
-
+form.addEventListener("submit", goodsUpdateApi);
