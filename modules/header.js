f5da61589de03bbd8275523dfb78ace253d3f3c8
younu-Yun <87592697+younu-Yun@users.noreply.github.com>
2023-04-27 00:11:47 +0900

    Refactor: 카테고리 기능수정

--- modules/header.js
+++ modules/header.js
@@ -1,5 +1,7 @@
 async function headerCategory() {
-    const response = await axios.get("http://34.64.112.166/api/admin_category");
+    const response = await axios.get(
+        "http://34.64.112.166/api/product/category/all"
+    );
 
     const categories = response.data;
     const categoryList = document.querySelector("nav > ul");
@@ -8,11 +10,16 @@ async function headerCategory() {
         .map(
             ({ category }) => `
             <li>
-                <a href="../goods/goods_list.html?category=${category}"><span>${category}</span></a>
+                <a href="/views/goods/goods_list.html?category=${category}"><span>${category}</span></a>
             </li>
         `
         )
         .join("");
+
+    const li = document.createElement("li");
+    li.innerHTML =
+        "<a href='/views/goods/goods_list.html'><span>전체</span></a>";
+    categoryList.prepend(li);
 }
 
 headerCategory();
--- views/goods/goods.js
+++ views/goods/goods.js
@@ -4,41 +4,76 @@ async function main() {
     const urlCategoryId = urlParams.get("category");
     console.log(urlCategoryId);
 
-    const response = await axios.get(
-        `http://34.64.112.166/api/product/category?category=${urlCategoryId}`
-    );
-
-    const products = response.data;
-    const productList = document.querySelector(".goods_list ul");
     const categoryTitle = document.querySelector(".category_title h2");
+    const productList = document.querySelector(".goods_list ul");
 
-    productList.innerHTML = products
-        .map(
-            ({
-                productId,
-                productName,
-                price,
-                imageUrl,
-                startDate,
-                endDate,
-            }) => `
-            <li>
-                <a href="./goods_view.html?productId=${productId}">
-                <div class="img_box">
-                    <img src="${imageUrl}" alt="${productName}" />
-                </div>
-                <div class="info_box">
-                    <span class="goods_date">${startDate} ~ ${endDate}</span>
-                    <strong class="goods_tit">${productName}</strong>
-                    <span class="goods_pri">${price.toLocaleString()}원</span>
-                </div>
-                </a>
-            </li>
-        `
-        )
-        .join("");
+    if (urlCategoryId !== null) {
+        const response = await axios.get(
+            `http://34.64.112.166/api/product/category?category=${urlCategoryId}`
+        );
 
-    categoryTitle.innerHTML = urlCategoryId;
+        const products = response.data;
+
+        productList.innerHTML = products
+            .map(
+                ({
+                    productId,
+                    productName,
+                    price,
+                    imageUrl,
+                    startDate,
+                    endDate,
+                }) => `
+                <li>
+                    <a href="./goods_view.html?productId=${productId}">
+                    <div class="img_box">
+                        <img src="${imageUrl}" alt="${productName}" />
+                    </div>
+                    <div class="info_box">
+                        <span class="goods_date">${startDate} ~ ${endDate}</span>
+                        <strong class="goods_tit">${productName}</strong>
+                        <span class="goods_pri">${price.toLocaleString()}원</span>
+                    </div>
+                    </a>
+                </li>
+            `
+            )
+            .join("");
+
+        categoryTitle.innerHTML = urlCategoryId;
+    } else {
+        const response = await axios.get(`http://34.64.112.166/api/product`);
+
+        const products = response.data;
+
+        productList.innerHTML = products
+            .map(
+                ({
+                    productId,
+                    productName,
+                    price,
+                    imageUrl,
+                    startDate,
+                    endDate,
+                }) => `
+                <li>
+                    <a href="./goods_view.html?productId=${productId}">
+                    <div class="img_box">
+                        <img src="${imageUrl}" alt="${productName}" />
+                    </div>
+                    <div class="info_box">
+                        <span class="goods_date">${startDate} ~ ${endDate}</span>
+                        <strong class="goods_tit">${productName}</strong>
+                        <span class="goods_pri">${price.toLocaleString()}원</span>
+                    </div>
+                    </a>
+                </li>
+            `
+            )
+            .join("");
+
+        categoryTitle.innerHTML = "전체상품";
+    }
 }
 
 main();
