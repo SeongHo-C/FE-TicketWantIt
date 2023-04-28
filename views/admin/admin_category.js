3c1fc4e09f490c4e067510becd6d948a0e82156c
seongho <leeseong7130@gmail.com>
2023-04-28 20:40:40 +0900

    Feat: 관리자 axios interceptor 연결

--- views/admin/admin_category.js
+++ views/admin/admin_category.js
@@ -1,35 +1,31 @@
-import { getToken } from "../../modules/token.js";
+import { isTokenExpired, tokenRefresh } from '../../modules/token.js';
+import instance from '../../modules/axios_interceptor.js';
 
-("use strict");
+('use strict');
 
 const currentDate = new Date();
-const options = { day: "numeric", month: "short", year: "numeric" };
+const options = { day: 'numeric', month: 'short', year: 'numeric' };
 const formattedDate = currentDate
-    .toLocaleDateString("en-US", options)
-    .toUpperCase();
+  .toLocaleDateString('en-US', options)
+  .toUpperCase();
 
-const date = document.querySelector(".date span");
+const date = document.querySelector('.date span');
 date.innerHTML = formattedDate;
 
-const categoryList = document.querySelector(".cate_list > ul");
+const categoryList = document.querySelector('.cate_list > ul');
 
 /* 상품목록리스트 */
 async function categoryApi() {
-    const response = await axios.get(
-        "http://34.64.112.166/api/admin_category",
-        {
-            headers: {
-                Authorization: `Bearer ${getToken()}`,
-            },
-        }
-    );
+  if (isTokenExpired()) tokenRefresh();
 
-    const categories = response.data;
-    console.log(categories);
+  const response = await instance.get('/api/admin_category');
 
-    categoryList.innerHTML = categories
-        .map(
-            ({ category, categoryId }) => `
+  const categories = response.data;
+  console.log(categories);
+
+  categoryList.innerHTML = categories
+    .map(
+      ({ category, categoryId }) => `
             <li data-id="${categoryId}">
             <input type="text" value="${category}">
             <button class="btn_cate_modify">
@@ -40,99 +36,87 @@ async function categoryApi() {
             </button>
         </li>
     `
-        )
-        .join("");
+    )
+    .join('');
 
-    // 카테고리 추가, 삭제
-    async function handleCategoryClick(e) {
-        e.preventDefault();
-        const categoryElem = e.target.closest("li");
-        const categoryId = categoryElem.dataset.id;
+  // 카테고리 추가, 삭제
+  async function handleCategoryClick(e) {
+    e.preventDefault();
+    const categoryElem = e.target.closest('li');
+    const categoryId = categoryElem.dataset.id;
 
-        if (e.target.classList.contains("btn_cate_delete")) {
-            try {
-                const response = await axios.delete(
-                    `http://34.64.112.166/api/admin_category/delete?categoryId=${categoryId}`,
-                    {
-                        headers: {
-                            Authorization: `Bearer ${getToken()}`,
-                        },
-                    }
-                );
+    if (e.target.classList.contains('btn_cate_delete')) {
+      try {
+        if (isTokenExpired()) tokenRefresh();
 
-                console.log("카테고리가 삭제되었습니다:", response);
+        const response = await instance.delete(
+          `/api/admin_category/delete?categoryId=${categoryId}`
+        );
 
-                alert("카테고리가 삭제되었습니다.");
-                location.reload();
-            } catch (error) {
-                console.error("카테고리 삭제 중 오류가 발생했습니다:", error);
-            }
-        } else if (e.target.classList.contains("btn_cate_modify")) {
-            const categoryInput = categoryElem.querySelector("input");
+        console.log('카테고리가 삭제되었습니다:', response);
 
-            try {
-                const response = await axios.put(
-                    `http://34.64.112.166/api/admin_category/edit?categoryId=${categoryId}`,
-                    {
-                        category: categoryInput.value,
-                    },
-                    {
-                        headers: {
-                            Authorization: `Bearer ${getToken()}`,
-                        },
-                    }
-                );
+        alert('카테고리가 삭제되었습니다.');
+        location.reload();
+      } catch (error) {
+        console.error('카테고리 삭제 중 오류가 발생했습니다:', error);
+      }
+    } else if (e.target.classList.contains('btn_cate_modify')) {
+      const categoryInput = categoryElem.querySelector('input');
 
-                console.log("해당 카테고리가 수정되었습니다:", response);
+      try {
+        if (isTokenExpired()) tokenRefresh();
 
-                alert("해당 카테고리가 수정되었습니다.");
-                location.reload();
-            } catch (error) {
-                console.error("카테고리 수정 중 오류가 발생했습니다:", error);
-            }
-        }
+        const response = await instance.put(
+          `/api/admin_category/edit?categoryId=${categoryId}`,
+          {
+            category: categoryInput.value,
+          }
+        );
+
+        console.log('해당 카테고리가 수정되었습니다:', response);
+
+        alert('해당 카테고리가 수정되었습니다.');
+        location.reload();
+      } catch (error) {
+        console.error('카테고리 수정 중 오류가 발생했습니다:', error);
+      }
     }
+  }
 
-    const list = document.querySelectorAll(".cate_list > ul > li");
+  const list = document.querySelectorAll('.cate_list > ul > li');
 
-    list.forEach((li) => {
-        const deleteButton = li.querySelector(".btn_cate_delete");
-        const modifyButton = li.querySelector(".btn_cate_modify");
+  list.forEach((li) => {
+    const deleteButton = li.querySelector('.btn_cate_delete');
+    const modifyButton = li.querySelector('.btn_cate_modify');
 
-        deleteButton.addEventListener("click", handleCategoryClick);
-        modifyButton.addEventListener("click", handleCategoryClick);
-    });
+    deleteButton.addEventListener('click', handleCategoryClick);
+    modifyButton.addEventListener('click', handleCategoryClick);
+  });
 }
 
 categoryApi();
 
-const addCategorybutton = document.querySelector(".btn_add_category");
+const addCategorybutton = document.querySelector('.btn_add_category');
 
 async function categoryAdd() {
-    const addCategoryInput = document.querySelector("#addCategory");
-    console.log(addCategoryInput.value);
+  const addCategoryInput = document.querySelector('#addCategory');
+  console.log(addCategoryInput.value);
 
-    if (addCategoryInput.value !== "") {
-        try {
-            const response = await axios.post(
-                "http://34.64.112.166/api/admin_category/add",
-                {
-                    category: addCategoryInput.value,
-                },
-                {
-                    headers: {
-                        Authorization: `Bearer ${getToken()}`,
-                    },
-                }
-            );
+  if (addCategoryInput.value !== '') {
+    try {
+      if (isTokenExpired()) tokenRefresh();
 
-            console.log("카테고리가 추가되었습니다.", response.data);
-            location.reload();
-            addCategoryInput.value = "";
-        } catch (error) {
-            console.error("카테고리가 추가에 실패했습니다", error);
-        }
+      const response = await instance.post('/api/admin_category/add', {
+        category: addCategoryInput.value,
+      });
+
+      console.log('카테고리가 추가되었습니다.', response.data);
+      location.reload();
+      addCategoryInput.value = '';
+    } catch (error) {
+      console.error('카테고리가 추가에 실패했습니다', error);
     }
+  }
 }
 
-addCategorybutton.addEventListener("click", categoryAdd);
+addCategorybutton.addEventListener('click', categoryAdd);
