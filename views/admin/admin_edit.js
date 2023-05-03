1869817bd1bc93a991a7d16bec5fb5a781d43108
younu-Yun <87592697+younu-Yun@users.noreply.github.com>
2023-05-04 02:20:14 +0900

    Refactor: 관리자 - 상품추가 파일경로 수정

--- views/admin/admin_edit.js
+++ views/admin/admin_edit.js
@@ -1,4 +1,4 @@
-import { isTokenExpired, tokenRefresh } from "../../modules/token.js";
+import { isTokenExpired, tokenRefresh, getToken } from "../../modules/token.js";
 import instance from "../../modules/axios_interceptor.js";
 
 ("use strict");
@@ -21,18 +21,10 @@ imageUrl.addEventListener("change", function () {
 });
 
 /*
-상품추가 작업순서:
-
-1. 상품추가 클릭 → goods_edit.html로 이동
-2. 정보값 입력 후 → 등록버튼 클릭
-3. 등록버튼 클릭시 이벤트로 10.10.6.158:5000/api/admin_product/add 에 POST로 값 보내기
-4. 상품관리 페이지로 이동하기
---------------------------
 
 상품수정 작업순서:
-
-1. 상품수정버튼 클릭 → goods_edit.html?productId 경로로 이동
-2. edit.html에 비어있는 값에 해당 상품상세정보 GET으로 가져와서 전체 삽입
+1. localStorage 말고 api에서 불러오기 
+2. 수정일때 해당 product데이터의 url값으로 file[0]에 맞는 obj를 만들어서 넣어놓기
 */
 
 const form = document.querySelector(".goods_form form");
@@ -50,64 +42,88 @@ const endDate = document.querySelector("#goodsDateEnd");
 const url = new URL(window.location.href);
 const urlParams = url.searchParams;
 const urlProductId = urlParams.get("productId");
+console.log(urlProductId);
 
-const product = JSON.parse(localStorage.getItem("product"));
+if (urlProductId !== null) {
+    const productModifyApi = async () => {
+        if (isTokenExpired()) await tokenRefresh();
 
-if (product !== null) {
-    category.value = product.category;
-    productName.value = product.name;
-    price.value = product.price;
-    description.value = product.description;
-    startDate.value = product.startDate;
-    endDate.value = product.endDate;
-    speciesAge.value = product.speciesAge;
-    place.value = product.place;
+        const response = await instance.get("/api/admin_product");
+        const products = response.data;
 
-    formUrlInput.value = product.image;
+        const filteredProduct = products.filter(
+            ({ productId }) => productId === urlProductId
+        );
+        console.log(filteredProduct);
+
+        const defaultUrl = filteredProduct[0].imageUrl;
+
+        async function convertURLtoFile(url) {
+            if (isTokenExpired()) await tokenRefresh();
+
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${getToken()}`,
+                },
+                responseType: "blob",
+            };
+
+            const response = await axios.get(url, config);
+            const blob = response.data;
+            const filename = url.split("/").pop();
+            const ext = filename.split(".").pop();
+            const type = `image/${ext}`;
+            return new File([blob], filename, { type });
+        }
+
+        const file = await convertURLtoFile(defaultUrl);
+        console.log(file);
+
+        /* 가짜 file_inputbox에 url경로만 넣어주기 */
+        formUrlInput.value = filteredProduct[0].imageUrl;
+        /* url을 file객체로 변환해서 실제 file_inputbox에 넣어주기 */
+        imageUrl.files[0] = file;
+        /* 기타 데이터 받아와서 빈 input값에 넣어주기 */
+        productName.value = filteredProduct[0].productName;
+        category.value = filteredProduct[0].category;
+        startDate.value = filteredProduct[0].startDate;
+        endDate.value = filteredProduct[0].endDate;
+        description.value = filteredProduct[0].description;
+        price.value = filteredProduct[0].price;
+        speciesAge.value = filteredProduct[0].speciesAge;
+        place.value = filteredProduct[0].place;
+
+        console.log(defaultUrl);
+    };
+
+    productModifyApi();
 }
-
-localStorage.removeItem("product");
 
 const goodsUpdateApi = async (e) => {
     e.preventDefault();
 
+    console.log(imageUrl.files[0]);
+    const formData = new FormData();
+
+    formData.append("category", category.value);
+    formData.append("productName", productName.value);
+    formData.append("imageUrl", imageUrl.files[0]);
+    formData.append("price", price.value);
+    formData.append("place", place.value);
+    formData.append("speciesAge", speciesAge.value);
+    formData.append("description", description.value);
+    formData.append("startDate", startDate.value);
+    formData.append("endDate", endDate.value);
+
     if (urlProductId !== null) {
         console.log("상품수정");
-
-        const updateApi = {
-            category: String(category.value),
-            productName: String(productName.value),
-            imageUrl: String(imageUrl.files[0]),
-            price: Number(price.value),
-            place: String(place.value),
-            speciesAge: String(speciesAge.value),
-            description: String(description.value),
-            startDate: String(startDate.value),
-            endDate: String(endDate.value),
-        };
-
-        const formData = new FormData();
-        formData.append("imageUrl", imageUrl.files[0]);
-
-        try {
-            if (isTokenExpired()) await tokenRefresh();
-
-            const responseUrl = await instance.put(
-                `/api/admin_product/edit/img?productId=${urlProductId}`,
-                formData
-            );
-
-            console.log("상품 이미지가 수정되었습니다:", responseUrl.data);
-        } catch (error) {
-            console.error("이미지 수정 중 오류가 발생했습니다:", error);
-        }
 
         try {
             if (isTokenExpired()) await tokenRefresh();
 
             const response = await instance.put(
-                `/api/admin_product/edit?productId=${urlProductId}`,
-                updateApi
+                `/api/admin_product/revise?productId=${urlProductId}`,
+                formData
             );
 
             console.log("상품이 수정되었습니다:", response.data);
@@ -118,18 +134,6 @@ const goodsUpdateApi = async (e) => {
         }
     } else {
         console.log("상품추가");
-
-        const formData = new FormData();
-
-        formData.append("category", category.value);
-        formData.append("productName", productName.value);
-        formData.append("imageUrl", imageUrl.files[0]);
-        formData.append("price", price.value);
-        formData.append("place", place.value);
-        formData.append("speciesAge", speciesAge.value);
-        formData.append("description", description.value);
-        formData.append("startDate", startDate.value);
-        formData.append("endDate", endDate.value);
 
         try {
             const response = await instance.post(
